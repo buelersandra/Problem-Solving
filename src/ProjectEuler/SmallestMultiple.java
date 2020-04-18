2c7aa4cea9996aaa285cc564127a91fdd5900b88
Sandra Konotey <buelersandra@gmail.com>
2020-04-18 03:25:13 +0000

    improved SmallestMultiple to run 2s~

--- src/ProjectEuler/SmallestMultiple.java
+++ src/ProjectEuler/SmallestMultiple.java
@@ -5,31 +5,49 @@ public class SmallestMultiple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		long startime = System.currentTimeMillis();
+
 		int answer = 1;
 		
-		int pass = 0;
-		
-		while(pass<20) {
-			pass=0;
-			for(int i=1;i<=20;i++) {
-				if(answer%i!=0) {
-					pass--;
-				}else {
-					pass++;
-				}
-			}
-//			if(answer%2==0 && answer%3==0 && answer%5==0 && answer%7==0 ) {
-//				pass = true;
-//				System.out.print(answer);
-//			}
-			
-				
-			System.out.println(String.format("pass is %d ; answer is %d", pass,answer));	
+		while(!hasPassed(answer)) {
 			answer++;
 		}
+		System.out.println(answer+" time is "+(System.currentTimeMillis()-startime));
+
+//	boolean pass = false;
+//	
+//		while(pass==false) {
+//			//pass=0;
+//			for(int i=1;i<=20;i++) {
+//				if(answer%i!=0) {
+//					pass=false;
+//					answer++;
+//					break;
+//					
+//				}else {
+//					pass=true;
+//				}
+//			}			
+//			
+//		}
+//		System.out.println(answer+" time is "+(System.currentTimeMillis()-startime));
+
+		//System.out.println(String.format(" answer is %d ; time is %d",answer,System.currentTimeMillis()-startime));	
 		
-		System.out.print(answer);
+		
 
+}
+	
+	public static boolean hasPassed(int answer) {
+		for(int i=1;i<=20;i++) {
+			if(answer%i!=0) 
+				return false;
+			
+		}
+		return true;
 	}
+	
+	
 
+		
 }
