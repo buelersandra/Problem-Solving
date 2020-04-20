ed885efe29d0ad2e48c02f24dd3cb59ffdc8140d
Sandra Konotey <buelersandra@gmail.com>
2020-04-20 14:21:13 +0000

    LC LongestSubstring

--- src/LeetCode/LongestSubstring.java
+++ src/LeetCode/LongestSubstring.java
@@ -3,8 +3,28 @@ package LeetCode;
 public class LongestSubstring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		lengthOfLongestSubstring("pwwkew");
 	}
+	//abcdd
+	public static int lengthOfLongestSubstring(String s) {
+		int max =0;
+		String nextChar="";
+		String newString = "";
+		for(int i =0;i<s.length();i++) {
+			for(int j =i+1;j<s.length();j++) {
+				nextChar = ""+s.charAt(j);
+				newString = s.substring(i,j);
+				if(newString.contains(nextChar)) {
+					max = Math.max(newString.length(), max);
+					break;
+				}
+			}
+			
+		}
+		System.out.print(max);
+		return max;
+        
+    }
 
 }
